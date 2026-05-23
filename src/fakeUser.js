517340df16cdece0f94fd28223413439592b8747
Versaillante <160363941+Versaillante@users.noreply.github.com>
2026-05-23 19:12:02 -0400

    Explore, FakeUser, Style, Applie
    files

---
+++ src/fakeUser.js
@@ -0,0 +1,14 @@
+const mockUser = {
+  id: 69696969,
+  display_name: "Sophie Tremblay",
+  email: "sophie.tremblay@email.com",
+  picture: "https://i.pravatar.cc/150?u=sophie",
+  sub: "auth0|mock123456",
+  discipline: ["performer"],
+  bio: "i'm a flute player who only does jazz",
+  appliedListings: ["123456", "78910"],
+  activeListings:["7777777"],
+  created_at: 2026-5-23,
+  updated_at: 2026-5-23,
+};
+
